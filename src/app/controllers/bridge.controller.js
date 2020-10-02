7665d287194f093950c8112e7ccddb7feeb6de87
aitizaz <atz.softprgmr@gmail.com>
2020-10-02 15:06:21 +0500

    bridge created

--- src/app/controllers/bridge.controller.js
+++ src/app/controllers/bridge.controller.js
@@ -1,33 +1,31 @@
-
 // including bridge model
-const { Bridge } = require('../models/bridge.model')
+const { Bridge } = require("../models/bridge.model");
 
 // Add bridged data to db
 const addBridgeData = async (req, res) => {
-    try {
+  try {
+    const { username, productchoice, userid, productid } = req.body;
 
-        const { username, productchoice, userid, productid } = req.body
+    const response = await Bridge.create({
+      username,
+      productchoice,
+      userid,
+      productid,
+    });
 
-        const response = await Bridge.create({
-            username,
-            productchoice,
-            userid,
-            productid
-        });
+    return res.status(200).json({
+      status: true,
+      data: response,
+    });
+  } catch (error) {
+    res.status(200).json({
+      status: true,
+      error: error.message,
+    });
+  }
+};
 
-        return res.status(200).json({
-            status: true,
-            data: response
-        })
-
-    } catch(error) {
-        res.status(200).json({
-            status: true,
-            error: error.message
-        })
-    }
-}
-
+// export model
 module.exports = {
-    addBridgeData
-}
+  addBridgeData,
+};
